77edc9390642cacf7ca74a23ddca36301ac8cfe7
Tamara Vera <tamarasoledad.vera96@gmail.com>
2024-02-24 22:26:53 -0300

    Solucionando validaciones de formulario

--- js/app.js
+++ js/app.js
@@ -25,65 +25,94 @@ document.addEventListener("DOMContentLoaded", function() {
 
 /*FORMULARIO CONTACTO*/
 //Captura elementos span(para mostrar error) por id
+let form = document.getElementById("form");
 let name_error = document.getElementById("name_error");
 let email_error = document.getElementById("email_error");
 let subject_error = document.getElementById("subject_error");
 let message_error = document.getElementById("message_error");
-let btn_submit = document.getElementById("btn_submit");
 
-//Agrega evento click al boton
-btn_submit.addEventListener("click", e => {
+//Agrega evento submit al formulario
+form.addEventListener("submit", e => {
     //Toma datos del formulario por id al momento de hacer click
     let name = document.getElementById("name");
     let email = document.getElementById("email");
     let subject = document.getElementById("subject");
     let message = document.getElementById("message"); 
-    //Evita envio del formulario
-    e.preventDefault();
+    
+    console.log(validateName(name));
+    console.log(validateEmail(email));
+    console.log(validateSubjectAndMessage(subject, message));
+    
+    if(!validateName(name) || !validateEmail(email) || !validateSubjectAndMessage(subject, message)){
+        // Vaciamos el campo
+        if(!validateName(name)){
+            name.value = "";
+        }
+        if(!validateEmail(email)){
+            email.value = "";
+        }
+        if(!validateSubjectAndMessage(subject, message)){
+            subject.value = "";
+            message.value = "";
+        }
+        //Evita envio del formulario
+        e.preventDefault();
+    }
 
-    validateName(name);
-    validateEmail(email);
-    validateSubjectAndMessage(subject, message);
+    let confirmation = document.querySelector(".confirmation");
+    confirmation.style.display = "block";
 })
 
-//Funciones - Validaciones
+//Funciones - Validaciones 
 const validateName = (name) => {
-    let validateName = /^[A-Za-zñÑáéíóúÁÉÍÓÚ\s]+$/;
-    //.value para tomar el valor del input y .length para saber cuantos caracteres tiene
+    let validateName = /^[A-Za-záéíóúñÁÉÍÓÚÑ\s]+$/; // Modificamos la expresión regular
+
     if (name.value === "" || name.value.length < 1) {
-        //Añade la clase de error al span capturado al comienzo
         name_error.classList.add("error");
-        //Modifica el valor del texto del span
         name_error.innerHTML = "Por favor ingresa tu nombre";
+        return false;
     }
-    //Método .test para validar regex (validateName)
-    else if (!validateName.test(name)){
+    // Verificamos si hay números en el nombre
+    if (!validateName.test(name.value)) {
         name_error.classList.add("error");
-        name_error.innerHTML = "No se permite números";
-        //Vacia el campo
-        name.value = "";
+        name_error.innerHTML = "No se permiten números en el nombre";
+        return false;
     }
+    return true;
 }
+
 const validateEmail = (email) => {
-    let validateEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    let validateEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
     if (email.value === "") {
         email_error.classList.add("error");
         email_error.innerHTML = "Por favor ingresa tu correo";
+
+        return false;
     }
-    else if(!validateEmail.test(email.value)){
+    if (!validateEmail.test(email.value)){
         email_error.classList.add("error");
         email_error.innerHTML = "Ingresa un correo valido";
+
+        return false;
     }
+
+    return true;
 };
 
 const validateSubjectAndMessage = (subject, message) => {
     if (subject.value === ""){
         subject_error.classList.add("error");
         subject_error.innerHTML = "No puede estar el campo vacio";
+
+        return false;
     }
     if (message.value === "") {
         message_error.classList.add("error");
         message_error.innerHTML = "No puede estar el campo vacio";  
+
+        return false;
     }
+
+    return true;
 }
